02b8d1cfd7caf1fa06a882e0bdfcef697fe9c0a1
kai.wang <kai.wang@baozun.com>
2021-02-24 09:54:21 +0800

    [feat]动态修改title

--- src/pages/list/router/index.js
+++ src/pages/list/router/index.js
@@ -1,25 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
-    component: () => import(/* webpackChunkName: "mcs-list" */ '../views/index.vue'),
+    path: "/",
+    name: "index",
+    component: () =>
+      import(/* webpackChunkName: "mcs-list" */ "../views/index.vue"),
+    meta: {
+      title: "index"
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "about"
+    }
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: "/list/",
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const {meta: { title }} = to;
+  document.title = title;
+  next();
+});
+
+export default router;
